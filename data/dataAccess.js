7562709a0261ff498bc677efe56426a616632ce3
Sam Collard <swift@swifteagle.co.uk>
2017-03-12 17:59:05 +0200

    Update neo4j driver

--- data/dataAccess.js
+++ data/dataAccess.js
@@ -1,14 +1,12 @@
 'use strict'
 
-var neo4j = require('neo4j')
+var neo4j = require('neo4j-driver').v1
+const DB_HOST = process.env.NEO4J_URL
 const DB_USER = process.env.NEO4J_USER
-const DB_PASS = process.env.NEO4J_PASS
-const DB_HOST = process.env.NEO4J_HOST
-var db = new neo4j.GraphDatabase(`http://${DB_USER}:${DB_PASS}@${DB_HOST}`)
-
-var Promise = require('bluebird')
-Promise.promisifyAll(db)
+const DB_PASS = process.env.NEO4J_PW
+var driver = neo4j.driver(`bolt://${DB_HOST}`, neo4j.auth.basic(DB_USER || 'neo4j', DB_PASS))
+var session = driver.session()
 
 exports.retrieveEntries = function() {
-  return db.cypherAsync({ query: 'MATCH (root:Root)-[rels:CONTAINS*]-(entries) RETURN root, rels, entries' })
+  return session.run('MATCH (root:Root)-[rels:CONTAINS*]-(entries:Entry) RETURN root, rels, entries')
 }
--- start.js
+++ start.js
@@ -1,4 +1,5 @@
 #!node
+
 'use strict'
 
 require('dotenv').config()
